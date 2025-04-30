e676a35d2ff7aeb72f5ede6543f6bd23d22e065a
Georgios Pitsos <gavintitanrider@gmail.com>
2025-04-30 17:23:14 +0200

    Issue #16
    Διορθώθηκε η κατανομή των ηλικιών. Εκκρεμεί να περιοριστεί το εύρος -> να γίνει πιο ρεαλιστικό, και για μάζα και για ηλικία.

--- GP_TRIALS/GP_TRIAL_1.jl
+++ GP_TRIALS/GP_TRIAL_1.jl
@@ -87,8 +87,8 @@ model = ABM(
 )
     
 for _ in 1:n_agents
-    age = rand(abmrng(model))*(age_range[2]-age_range[1])
-    mass = rand(abmrng(model)) * (mass_range[2]-mass_range[1]) +mass_range[1]
+    age = rand(abmrng(model))*(age_range[2]-age_range[1]) + age_range[1]
+    mass = rand(abmrng(model)) * (mass_range[2]-mass_range[1]) + mass_range[1]
     vel = Tuple(rand(abmrng(model), 2) .* (speed_range[2]-speed_range[1]) .+ speed_range[1])
     pos = Tuple((rand(abmrng(model), floor.(ag_range_y)), rand(abmrng(model), floor.(ag_range_x))))
     person = add_agent!(pos, AgentEscapes, model, vel, age, mass, 1., [pos[1]], [pos[2]], [0.0], [0.0], [0.0])
